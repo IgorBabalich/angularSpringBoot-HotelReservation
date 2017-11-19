873f1a045e273d172d0e822448fc9da6745eb55b
Igor Babalich <igor.babalich@gmail.com>
2017-11-19 07:39:15 -0500

    Adding Java Configuration

--- src/main/java/hotel/hotelreservation/HotelReservationApplication.java
+++ src/main/java/hotel/hotelreservation/HotelReservationApplication.java
@@ -1,9 +1,13 @@
 package hotel.hotelreservation;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class HotelReservationApplication {
 
 	public static void main(String[] args) {
